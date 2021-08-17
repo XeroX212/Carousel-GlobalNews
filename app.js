d591b47f7bea33c0b49d89921ef2a93924962597
Varun Chadha <arunpreet37@gmail.com>
2021-08-16 23:18:08 -0400

    fiddling with jason array

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 app.listen(port, () => console.log(`Listening on port ${port}`))
 
-// Routes
+//Routes
 const newsRouter = require('./src/routes/index')
 
 app.use('/', newsRouter)
@@ -18,11 +18,16 @@ app.use('/', newsRouter)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.urlencoded({ extended : true }))
+
+//Static Files
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 
 // Templating Engine
 app.set('views', './src/views')
 app.set('view engine', 'ejs')
+
 
 function normalizePort(val) {
     const port = parseInt(val, 10)
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,19 +4,19 @@ const axios = require('axios')
 
 newsRouter.get('', async(req, res) => {
     try {
-        const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts/`)
-        res.render('index', { articles : newsAPI.data })
+        const GlobalNewsAPI = await axios.get(`https://globalnews.ca/gnca-ajax-redesign/sample-data/`)
+        res.render('index', { cards : GlobalNewsAPI.data })
     } catch (err) {
         if(err.response) {
-            res.render('index', { articles : null })
+            res.render('index', { cards : null })
             console.log(err.response.data)
             console.log(err.response.status)
             console.log(err.response.headers)
         } else if(err.requiest) {
-            res.render('index', { articles : null })
+            res.render('index', { cards : null })
             console.log(err.requiest)
         } else {
-            res.render('index', { articles : null })
+            res.render('index', { cards : null })
             console.error('Error', err.message)
         }
     } 
